7289528dae40f788c162f6ba9e50bfa4179116bb
Trần Nguyện <kidnguyenkun@gmail.com>
2020-02-12 22:15:48 +0700

    add simple testing

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  wait,
+} from '@testing-library/dom'
 import App from './App';
 
 test('renders learn react link', () => {
@@ -7,3 +14,16 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Simple testing', async () => {
+  const textValue = 'Tran Nguyen'
+  const container = App()
+
+  const input = getByLabelText(container, 'name');
+  input.value = textValue
+
+  expect(getByLabelText(container, 'name').value).toBe(
+    textValue
+  )
+  expect(container).toMatchSnapshot()
+})
